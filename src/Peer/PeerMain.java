c088a0e51b6c89522ff7b98b289d42c5abc0dfb7
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2018-03-09 13:58:53 +0000

    added class for calculating the fileID field

--- src/Peer/PeerMain.java
+++ src/Peer/PeerMain.java
@@ -5,21 +5,36 @@ import java.nio.file.FileStore;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class PeerMain {
-    public static void main(String args[]){
-        for (Path root : FileSystems.getDefault().getRootDirectories()) {
-            System.nanoTime();
-            System.out.print(root + ": ");
-            try {
-                FileStore store = Files.getFileStore(root);
-                double available = (store.getUsableSpace()/(Math.pow(10f,9f)));
-                double totalS = (store.getTotalSpace()/(Math.pow(10f,9f)));
-                System.out.println("available=" + available
-                        + ", total=" + totalS);
-            } catch (IOException e) {
-                System.out.println("error querying space: " + e.toString());
-            }
-        }
+  public static void main(String args[]) throws NoSuchAlgorithmException {
+    for (Path root : FileSystems.getDefault().getRootDirectories()) {
+      System.nanoTime();
+      System.out.print(root + ": ");
+      try {
+        FileStore store = Files.getFileStore(root);
+        double available = (store.getUsableSpace() / (Math.pow(10f, 9f)));
+        double totalS = (store.getTotalSpace() / (Math.pow(10f, 9f)));
+        System.out.println("available=" + available + ", total=" + totalS);
+      } catch (IOException e) {
+        System.out.println("error querying space: " + e.toString());
+      }
     }
+      String str = "test";
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodedhash =
+              digest.digest(str.getBytes());
+
+      StringBuffer hexString = new StringBuffer();
+      for (int i = 0; i < encodedhash.length; i++) {
+          String hex = Integer.toHexString(0xff & encodedhash[i]);
+          if(hex.length() == 1) hexString.append('0');
+          hexString.append(hex);
+      }
+
+      System.out.print("SHA-256 Hash: "+hexString.toString());
+
+  }
 }
