e95f171d71aef21020967403863932f0fc5aa9c3
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2018-03-09 14:27:49 +0000

    added class tasked with sha256 hashing

--- src/Peer/PeerMain.java
+++ src/Peer/PeerMain.java
@@ -22,19 +22,5 @@ public class PeerMain {
         System.out.println("error querying space: " + e.toString());
       }
     }
-      String str = "test";
-      MessageDigest digest = MessageDigest.getInstance("SHA-256");
-      byte[] encodedhash =
-              digest.digest(str.getBytes());
-
-      StringBuffer hexString = new StringBuffer();
-      for (int i = 0; i < encodedhash.length; i++) {
-          String hex = Integer.toHexString(0xff & encodedhash[i]);
-          if(hex.length() == 1) hexString.append('0');
-          hexString.append(hex);
-      }
-
-      System.out.print("SHA-256 Hash: "+hexString.toString());
-
   }
 }
---
+++ src/Utils/Hashing.java
@@ -0,0 +1,58 @@
+package Utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hashing{
+
+    /**
+     *
+     * @param string
+     * @return
+     */
+    private static byte[] stringToSHA256Bytes(String string) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return digest.digest(string.getBytes());
+    }
+
+    /**
+     *
+     * @param encodedhash
+     * @return
+     */
+    private static String SHA256ByteArrayToString(byte[] encodedhash){
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < encodedhash.length; i++) {
+            String hex = Integer.toHexString(0xff & encodedhash[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    /**
+     *
+     * @param string
+     * @return
+     */
+    private static String hashToSHA256(String string){
+        byte[] bytes    = stringToSHA256Bytes(string);
+        String sha256String   = SHA256ByteArrayToString(bytes);
+        return sha256String;
+    }
+
+    /**
+     *
+     * @param string
+     * @return
+     */
+    public static String hashWithSHA256(String string) {
+        return hashToSHA256(string);
+    }
+
+}
