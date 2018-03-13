354820965a741d703f72c28b1475a492d9849208
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2018-03-13 23:17:51 +0000

    added unit tests for the file division spacial requirements calculator and generator methods in the the FileDivider Class

--- src/Tests/FileHandlerTest/FileDividerTests.java
+++ src/Tests/FileHandlerTest/FileDividerTests.java
@@ -1,4 +1,79 @@
 package Tests.FileHandlerTest;
 
-public class FileDividerTests {
+import FileHandler.FileDivider;
+import javafx.util.Pair;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileDividerTests extends FileDivider {
+
+    public FileDividerTests() {
+        super(5);
+    }
+
+    @Test
+    public void chunkSizeRequirementCalculatorAllLargeTest() {
+        int testSize = 10;
+
+        Pair<Integer, Integer> result =
+                this.calculateFileChunkSizeRequirement(testSize);
+
+        assertEquals(2, result.getKey().intValue());
+        assertEquals(0, result.getValue().intValue());
+    }
+
+    @Test
+    public void chunkSizeRequirementCalculatorLargeAndSmallTest() {
+        int testSize = 13;
+
+        Pair<Integer, Integer> result =
+                this.calculateFileChunkSizeRequirement(testSize);
+
+        assertEquals(2, result.getKey().intValue());
+        assertEquals(1, result.getValue().intValue());
+    }
+
+    @Test
+    public void chunkSizeRequirementCalculatorAllSmallTest() {
+        int testSize = 3;
+
+        Pair<Integer, Integer> result =
+                this.calculateFileChunkSizeRequirement(testSize);
+
+        assertEquals(0, result.getKey().intValue());
+        assertEquals(1, result.getValue().intValue());
+    }
+
+    @Test
+    public void chunkContainerGeneratorAllLargeTest() {
+        int testSize = 10;
+
+        String[] result = this.fileChunkContainerGenerator(testSize);
+
+        assertEquals(2, result.length);
+    }
+
+    @Test
+    public void chunkContainerGeneratorAllSmallTest() {
+        int testSize = 3;
+
+        String[] result = this.fileChunkContainerGenerator(testSize);
+
+        assertEquals(1, result.length);
+    }
+
+    @Test
+    public void chunkContainerGeneratorSmallAndLargeTest() {
+        int testSize = 13;
+
+        String[] result = this.fileChunkContainerGenerator(testSize);
+
+        assertEquals(3, result.length);
+    }
+    
+    @Test
+    public void fileDivisionAllLargeTest(){
+        
+    }
 }
