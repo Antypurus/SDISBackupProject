06809fd0484a96f68bb68a448d749f6b2e182540
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2018-03-16 11:39:49 +0000

    added unit tests for the file division spacial requirements calculator and generator methods in the the FileDivider Class

--- src/FileHandler/FileMetadataObtainer.java
+++ src/FileHandler/FileMetadataObtainer.java
@@ -1,4 +1,46 @@
 package FileHandler;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
 public class FileMetadataObtainer {
+
+    private String filepath = "";
+    private Path file = null;
+    private BasicFileAttributes attributes = null;
+
+    /**
+     *
+     * @param filepath
+     * @throws IOException
+     */
+    public FileMetadataObtainer(String filepath) throws IOException {
+        this.filepath = filepath;
+        this.file = Paths.get(filepath);
+        this.attributes = Files.readAttributes(this.file,BasicFileAttributes.class);
+    }
+
+    /**
+     *
+     * @param filepath
+     * @throws IOException
+     */
+    public void changeFilePath(String filepath) throws IOException {
+        this.filepath = filepath;
+        this.file = Paths.get(filepath);
+        this.attributes = Files.readAttributes(this.file,BasicFileAttributes.class);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public FileTime getFileCreationTime(){
+        return this.attributes.creationTime();
+    }
+
 }
