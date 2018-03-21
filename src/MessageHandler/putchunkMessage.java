dfc4678f8c07ecf356352752ec3ab55ebb49de55
Tiago José Sousa Magalhães <up201607931@fe.up.pt>
2018-03-21 13:57:17 +0000

    updated putchunk message class to contain the replication degree and generate message header

--- src/MessageHandler/putchunkMessage.java
+++ src/MessageHandler/putchunkMessage.java
@@ -6,17 +6,20 @@ import java.io.IOException;
 
 public class putchunkMessage extends Message {
 
+    private int replicationDeg;
+
     /**
      *
      * @param senderID
      * @param chunkNO
      * @param FileID
      */
-    public putchunkMessage(int senderID,int chunkNO,String FileID){
+    public putchunkMessage(int senderID,int chunkNO,String FileID,int replicationDeg){
         this.messageType = "PUTCHUNK";
         this.senderID = senderID;
         this.chunkNO = chunkNO;
         this.FileID = FileID;
+        this.replicationDeg = replicationDeg;
     }
 
     /**
@@ -26,16 +29,12 @@ public class putchunkMessage extends Message {
      * @param startChunk
      * @param filename
      */
-    public putchunkMessage(int senderID,int chunkNO,String startChunk,String filename) throws IOException {
+    public putchunkMessage(int senderID,int chunkNO,String startChunk,String filename,int replicationDeg) throws IOException {
         this.messageType = "PUTCHUNK";
         this.senderID = senderID;
         this.chunkNO = chunkNO;
+        this.replicationDeg = replicationDeg;
         this.FileID = this.calculateFileID(startChunk,filename);
-    }
-
-    @Override
-    public String toString() {
-        return null;
     }
 
     /**
@@ -48,5 +47,33 @@ public class putchunkMessage extends Message {
     private String calculateFileID(String startChunk,String Filename) throws IOException {
         FileIDCalculator cal = new FileIDCalculator(startChunk,Filename,this.senderID);
         return cal.calculateFileID();
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString(){
+        String ret = this.messageType + " " + this.protocolVersion;
+        ret += " " + this.senderID + " " + this.FileID + " ";
+        ret += this.chunkNO + " " + this.replicationDeg;
+        return ret;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getReplicationDeg() {
+        return replicationDeg;
+    }
+
+    /**
+     *
+     * @param replicationDeg
+     */
+    public void setReplicationDeg(int replicationDeg) {
+        this.replicationDeg = replicationDeg;
     }
 }
