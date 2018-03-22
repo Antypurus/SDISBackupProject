ec2c023d08a6af28f68ea2942b6e883fe04d549a
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-03-22 22:51:06 +0000

    added a multicast socket to the putchunk sub protocol

--- src/initiatorPeerSubprotocols/putchunkSubprotocol.java
+++ src/initiatorPeerSubprotocols/putchunkSubprotocol.java
@@ -5,11 +5,13 @@ import FileHandler.FileStreamer;
 import Utils.threadRegistry;
 
 import java.io.IOException;
+import java.net.MulticastSocket;
 
 public class putchunkSubprotocol {
 
     private final int CHUNK_BODY_SIZE = 64000;
 
+    private MulticastSocket socket;
     private String          startChunk;
     private String          filepath;
     private int             senderID;
@@ -21,11 +23,14 @@ public class putchunkSubprotocol {
      * @param senderID
      * @param filepath
      * @param registry
+     * @param socket
      * @throws IOException
      */
-    public putchunkSubprotocol(int senderID, String filepath, threadRegistry registry) throws IOException {
+    public putchunkSubprotocol(int senderID, String filepath, threadRegistry registry, MulticastSocket socket) throws IOException {
         this.filepath = filepath;
         this.senderID = senderID;
+        this.registry = registry;
+        this.socket = socket;
 
         FileStreamer stream = new FileStreamer(this.CHUNK_BODY_SIZE,filepath);
         this.startChunk = stream.read();
