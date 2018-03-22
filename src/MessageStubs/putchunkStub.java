910b681a1fb4de3612962bac252fbf1687d0147a
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-03-22 22:45:02 +0000

    defined message stub interface specification

--- src/MessageStubs/putchunkStub.java
+++ src/MessageStubs/putchunkStub.java
@@ -1,4 +1,30 @@
 package MessageStubs;
 
-public class putchunkStub {
+import MessageHandler.Message;
+
+public class putchunkStub implements MessageStub,Runnable {
+    @Override
+    public synchronized void addInboundMessage(Message message) {
+
+    }
+
+    @Override
+    public synchronized  Message getInboundMessage() {
+        return null;
+    }
+
+    @Override
+    public int checkUnprocessedMessages() {
+        return 0;
+    }
+
+    @Override
+    public String checkStubStatus() {
+        return null;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
