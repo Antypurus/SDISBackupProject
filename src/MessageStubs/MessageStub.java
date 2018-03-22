1407ee52aaec164c164bd936c5e89a07de1c7d9d
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-03-22 22:37:53 +0000

    defined message stub interface specification

--- src/MessageStubs/MessageStub.java
+++ src/MessageStubs/MessageStub.java
@@ -1,4 +1,12 @@
 package MessageStubs;
 
+import MessageHandler.Message;
+import java.util.Queue;
+
 public interface MessageStub{
+    Queue<Message> incomingQueu = null;
+    void addInboundMessage(Message message);
+    Message getInboundMessage();
+    int checkUnprocessedMessages();
+    String checkStubStatus();
 }
