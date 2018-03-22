d506db0b4c0563ea20c0c0645703a6b8011f0d0f
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-03-22 22:52:51 +0000

    added a multicast socket to the putchunk sub protocol

--- src/MessageStubs/MessageStub.java
+++ src/MessageStubs/MessageStub.java
@@ -1,10 +1,15 @@
 package MessageStubs;
 
 import MessageHandler.Message;
+
+import java.net.MulticastSocket;
 import java.util.Queue;
 
 public interface MessageStub{
-    Queue<Message> incomingQueu = null;
+    Queue<Message>          incomingQueu = null;
+    MulticastSocket         sockre = null;
+    String                  status = null;
+
     void addInboundMessage(Message message);
     Message getInboundMessage();
     int checkUnprocessedMessages();
