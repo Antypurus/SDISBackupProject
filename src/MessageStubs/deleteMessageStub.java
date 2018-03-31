fd6251bf106b5f3c9bb61a72bc09d3959f1e70d8
TiagoJSMagalhaes <Crakylps@gmail.com>
2018-03-31 18:29:50 +0100

    added delete stub method

--- src/MessageStubs/deleteMessageStub.java
+++ src/MessageStubs/deleteMessageStub.java
@@ -1,22 +1,46 @@
 package MessageStubs;
 
+import MessageHandler.deleteMessage;
+import fileDatabase.backedUpFileData;
+import fileDatabase.backedUpFileDatabase;
+import MessageHandler.Message;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
-public class deleteMessageStub {
+public class deleteMessageStub implements Runnable{
 
     private MulticastSocket socket;
     private InetAddress address;
     private int port;
     private int senderId;
-    private String fileId;
+    private String filename;
 
-    public  deleteMessageStub(MulticastSocket socket, InetAddress address,int port,int senderId,String fileId){
+    public  deleteMessageStub(MulticastSocket socket, InetAddress address,int port,int senderId,String filename){
         this.senderId = senderId;
         this.socket = socket;
         this.address = address;
         this.port = port;
-        this.fileId = fileId;
+        this.filename = filename;
     }
 
+
+    @Override
+    public void run() {
+        backedUpFileDatabase db = backedUpFileDatabase.getDatabase();
+        backedUpFileData data = db.getRegisteredBackedUpFileData(this.filename);
+
+        if(data!=null) {
+            Message message = new deleteMessage(this.senderId, data.getFileId());
+            byte[] send = message.toString().getBytes();
+            DatagramPacket packet = new DatagramPacket(send,send.length,this.address,this.port);
+            try {
+                this.socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
